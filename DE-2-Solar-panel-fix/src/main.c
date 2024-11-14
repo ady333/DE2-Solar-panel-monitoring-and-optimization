eafde37dadbb40d2e29c4645721d980a93201348
Raddyn <radoslav.tomcala@gmail.com>
2024-11-14 11:38:20 +0100

    changed becasue of human error

---
+++ DE-2-Solar-panel-fix/src/main.c
@@ -0,0 +1,31 @@
+/*
+Description place holder
+*/
+
+// --Includes-------------------------------------------------------------------//
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+
+
+// --Defines--------------------------------------------------------------------//
+
+// --Variables------------------------------------------------------------------//
+
+// --Function definitions-------------------------------------------------------//
+
+int main(void)
+{
+
+}
+
+// -- Interrupt service routines ------------------------------------------------//
+
+
+
+
+// -- end of file --//
